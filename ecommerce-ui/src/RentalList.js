b1c766966dd4a5509ce2a7bd5e91fdc65cb60613
Michael S. Hoffman <mikeshoffman@gmail.com>
2019-06-10 22:28:06 -0700

    Add PropTypes for props so far

--- ecommerce-ui/src/RentalList.js
+++ ecommerce-ui/src/RentalList.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import Rental from './Rental.generic';
+import PropTypes from 'prop-types';
 
 export default class RentalList extends React.Component {
-    render() {
-        const rentalList = this.props.rentals
-            .map((rental, idx) => {
-                return (
-                    <Rental
-                        rental={rental}
-                        onClick={this.props.onBookRental}
-                        idx={idx}
-                        key={idx}
-                        buttonText="Book"
-                    />
-                );
-            });
+  static propTypes = {
+    rentals: PropTypes.array.isRequired,
+    onBookRental: PropTypes.func.isRequired,
+  }
 
+  render() {
+    const rentalList = this.props.rentals
+      .map((rental, idx) => {
         return (
-            <div>
-                <ul>
-                    {rentalList}
-                </ul>
-            </div>
-        );
-    }
+          <Rental
+            rental={rental}
+            onClick={this.props.onBookRental}
+            idx={idx}
+            key={idx}
+            buttonText="Book"
+        />
+      );
+    });
+
+    return (
+      <div>
+        <ul>
+          {rentalList}
+        </ul>
+    </div>
+  );
+  }
 }
--- ecommerce-ui/src/ShoppingCart.js
+++ ecommerce-ui/src/ShoppingCart.js
@@ -1,26 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ShoppingCart extends React.Component {
-    render() {
-        const rentalList = this.props.rentals
-            .map((rental, idx) => {
-                return (
-                    <li key={idx}>
-                        <span>{rental}</span>
-                        <button onClick={() => this.props.onDeleteRental(idx)}>Delete</button>
-                    </li>
-                );
-            });
+  static propTypes = {
+    rentals: PropTypes.array.isRequired,
+    onDeleteRental: PropTypes.func.isRequired,
+  }
 
+  render() {
+    const rentalList = this.props.rentals
+      .map((rental, idx) => {
         return (
-            <div>
-                <h2>Shopping Cart</h2>
-                <ul>
-                    {rentalList}
-                </ul>
-            </div>
+          <li key={idx}>
+            <span>{rental}</span>
+            <button onClick={() => this.props.onDeleteRental(idx)}>Delete</button>
+          </li>
         );
-    }
+      });
+
+    return (
+      <div>
+        <h2>Shopping Cart</h2>
+        <ul>
+          {rentalList}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default ShoppingCart;
